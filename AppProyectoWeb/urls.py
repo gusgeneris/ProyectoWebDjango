54a1b0bef89c257a708d78070a8c1e71699a03da
gusgeneris <gusgeneris92@gmail.com>
2022-02-03 23:37:23 -0300

    Aplicacion de boostrap, creacion de template base.

--- AppProyectoWeb/urls.py
+++ AppProyectoWeb/urls.py
@@ -3,9 +3,9 @@ from AppProyectoWeb import views
 
 
 urlpatterns = [
-    path('', views.home, name='Inicio'),
-    path('servicios/', views.servicios),
-    path('tienda/', views.tienda),
-    path('blog/', views.blog),
-    path('contacto/', views.contacto),
+    path('', views.home, name='Home'),
+    path('servicios/', views.servicios, name='Servicios'),
+    path('tienda/', views.tienda, name='Tienda'),
+    path('blog/', views.blog, name='Blog'),
+    path('contacto/', views.contacto, name='Contacto'),
 ]
